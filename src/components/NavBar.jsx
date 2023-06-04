8d9cbe83216001c94835b19d85d9e2059cc87773
Zorahidaconh <121045016+Zorahida@users.noreply.github.com>
2023-06-04 18:29:32 +0200

    Update css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom"
 
 function NavBar() {
     return(
-        <nav className="Nav">
-        <div>
+        <nav>
+        <div className="Nav">
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/character">Personajes</Link></li>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,22 @@
 import Footer from '../components/Footer';
 import '../styles/App.css';
 
-function Home({ Home }) {
-
-    return <>
-    <header className='Header'>
+function Home( ) {
+    return (
+    <>
+    <header className='header'>
     <h1>Rick&Morty Adventures {Home}</h1>
     </header>
     <main className='Main'>
     <div >
-       <p>Estas con las locas hstorias de Rick & Morty</p>
+       <p>Estas son las locas hstorias de Rick & Morty</p>
         </div>
     </main>
-    <footer className='Footer'>
-        <Footer Home={Home}/>
+    <footer className='footer'>
+        <Footer/>
     </footer>
     </>
+    );
 }
 
 export default Home
